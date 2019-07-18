eb7dc0d226200a57a74c333892fd1c783906cd34
lyndaruiz95 <lyndaruiz95@gmail.com>
2019-07-17 20:17:05 -0700

    Added home page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { RetroboardComponent } from './retroboard/retroboard.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  // Q: Is there a way to add app.component to be the default home page?
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  { 
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   { 
     path:'retroboard', 
-    component:RetroboardComponent
+    component: RetroboardComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CreateNewBoardComponent } from './create-new-board/create-new-board.component';
 import { RetroboardComponent } from './retroboard/retroboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateNewBoardComponent,
-    RetroboardComponent
+    RetroboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
